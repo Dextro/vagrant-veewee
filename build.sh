25b6623385bcad549bb6ea2f4299b4a7aced6a12
Bert Pattyn <bert@dextrose.be>
2013-04-21 09:27:56 +0200

    add rvm to build script

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ gem install bundler
 
 bundle install
 
+rvm install 1.9.3
+rvm use 1.9.3
+
 bundle exec veewee vbox build 'debian-606' --force --auto --nogui
 bundle exec veewee vbox validate 'debian-606'
 
